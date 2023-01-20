f00ce2c121372bab7f6cd4b2ca342e1b238f3c69
AlisonForsberg <aforsberg95@gmail.com>
2023-01-20 13:39:42 -0700

    Furthered token work

--- server.js
+++ server.js
@@ -6,16 +6,25 @@ const port = 3000;
 
 const bodyParser = require ('body-parser');
 
+const {v4: uuidv4} = require('uuid');
+
 app.use(bodyParser.json()) ;                                           //Looks for incoming data
 
 app.get ("/", (req, res) => {
-    res.send("Hello Alison")
+    res.send("Hello Alison");
 });
 
 app.post('/login', (req, res) =>{                                       //Allows us to send data          /login tells you what page it is
-    const loginUser = req.body.userName;                                //
-    CSSConditionRule.log('Login username:' +loginUser);                 //What we are requesting - it is logged
-    res.send('Hello'+loginUser);                                        //
+    const loginUser = req.body.userName;    
+    const loginPassword = req.body.password;
+    console.log('Login username: '+loginUser);
+    if (loginUser=="yahoo@gmail.com" && loginPassword=="P@ssw0rd"){                            //
+        const loginToken = uuidv4();
+        res.send(loginToken);
+    } else {
+        res.status(401);
+        res.send('Incorrect password for '+loginUser);
+    }                                      
 });
 
 app.listen(port, () => {
